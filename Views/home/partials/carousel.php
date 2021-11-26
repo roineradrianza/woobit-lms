28c41ed5bea29346c3e7be90f84545246fcec147
Roiner Adrianza <roineradrianzap@gmail.com>
2021-11-25 20:48:22 -0400

    Slider changed to one static image on homepage

--- Views/home/partials/carousel.php
+++ Views/home/partials/carousel.php
@@ -1,5 +1,5 @@
 <v-carousel v-model="carousel" class="no-ha" :show-arrows="false" cycle hide-delimiters>
-    <?php for ($i = 1; $i <= 4; $i++): ?>
+    <?php for ($i = 1; $i <= 1; $i++): ?>
     <v-carousel-item>
         <?= new Controller\Template("home/partials/slides/$i") ?>
     </v-carousel-item>
