6ba8c7733156621d92b9624c238376c545852000
Roiner Adrianza <roineradrianzap@gmail.com>
2021-12-05 02:50:21 -0400

    Logo added to admin sidebar

--- Views/admin/parts/sidebar.php
+++ Views/admin/parts/sidebar.php
@@ -1,18 +1,21 @@
-							 
-			  <v-navigation-drawer class="sidebar" v-model="drawer" app>
-			    <v-list dense>
-			      <v-subheader class="subtitle-1 white--text">Dashboard</v-subheader>
-			      <v-list-item-group v-model="selectedItem" color="primary">
-			      	<?php foreach ($data['tabs'] as $i => $tab): ?>
-				        <v-list-item key="<?= $i ?>" href="<?= SITE_URL . '/admin/'. $tab['url'] ?>" link>
-				          <v-list-item-icon>
-				            <v-icon class="white--text"><?= $tab['icon'] ?></v-icon>
-				          </v-list-item-icon>
-				          <v-list-item-content>
-				            <v-list-item-title class="white--text"><?= $tab['name'] ?></v-list-item-title>
-				          </v-list-item-content>
-				        </v-list-item>
-			      	<?php endforeach ?>
-			      </v-list-item-group>
-			    </v-list>
-			  </v-navigation-drawer>
+<v-navigation-drawer class="sidebar" v-model="drawer" app>
+    <v-list dense>
+        <div class="d-flex justify-center">
+            <v-img src="<?= SITE_URL ?>/img/woobit-white-logo.png" max-width="100px"></v-img>
+
+        </div>
+        <v-subheader class="subtitle-1 white--text">Dashboard</v-subheader>
+        <v-list-item-group v-model="selectedItem" color="primary">
+            <?php foreach ($data['tabs'] as $i => $tab): ?>
+            <v-list-item key="<?= $i ?>" href="<?= SITE_URL . '/admin/'. $tab['url'] ?>" link>
+                <v-list-item-icon>
+                    <v-icon class="white--text"><?= $tab['icon'] ?></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-title class="white--text"><?= $tab['name'] ?></v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <?php endforeach ?>
+        </v-list-item-group>
+    </v-list>
+</v-navigation-drawer>
