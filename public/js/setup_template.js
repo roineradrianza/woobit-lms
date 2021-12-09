4cc34c5f475c7ec73fc70814269c539c81228257
Roiner Adrianza <roineradrianzap@gmail.com>
2021-12-09 00:56:40 -0400

    Adjustments

--- public/js/setup_template.js
+++ public/js/setup_template.js
@@ -2,10 +2,10 @@ const domain = window.location.origin
 const current_url = window.location.href
 const url_params = new URLSearchParams(window.location.search);
 const api_url = domain + "/api/"
-const google_api_key = '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'
-const google_client_key = "815646876780-esiladq6m2oak54mier5r6upt2qa4kuj.apps.googleusercontent.com"
-const fb_app_id = '701132754096805'
-const fb_secret_key = "44cf119ba1352cc9a0aa2700890850f3"
+const google_api_key = ''
+const google_client_key = ""
+const fb_app_id = ''
+const fb_secret_key = ""
 let date =  new Date()
 const current_date = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
 /*VUE PLUGINS*/
