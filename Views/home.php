d7652b3946db38c9433e793d8684983516e49cfd
Roiner Adrianza <roineradrianzap@gmail.com>
2021-12-24 00:26:06 -0400

    Home updates

--- Views/home.php
+++ Views/home.php
@@ -3,38 +3,34 @@
 </v-row>
 
 <?= new Controller\Template('home/partials/how-works') ?>
-<v-container id="container_1" class="px-4 px-md-16 d-flex justify-center mb-16">
-    <v-row>
-        <v-col cols="12">
-            <v-row>
-                <v-col cols="12" md="4">
-                    <v-card color="primary" flat>
-                        <v-card-text class="px-0 py-0">
-                            <h3 class="text-h4 text-center white--text">Cursuri</h3>
-                            <v-img src="https://picsum.photos/1200/720?random=<?= rand(0, 10000) ?>" max-width="100vw">
-                            </v-img>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-                <v-col cols="12" md="4">
-                    <v-card color="primary" flat>
-                        <v-card-text class="px-0 py-0">
-                            <h3 class="text-h4 text-center white--text">Lectori</h3>
-                            <v-img src="https://picsum.photos/1200/720?random=<?= rand(0, 10000) ?>" max-width="100vw">
-                            </v-img>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-                <v-col cols="12" md="4">
-                    <v-card color="primary" flat>
-                        <v-card-text class="px-0 py-0">
-                            <h3 class="text-h4 text-center white--text">Despre noi</h3>
-                            <v-img src="https://picsum.photos/1200/720?random=<?= rand(0, 10000) ?>" max-width="100vw">
-                            </v-img>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
-            </v-row>
+<v-container class="px-4 px-md-16 mb-16">
+    <v-row justify="center">
+        <v-col cols="12" md="4">
+            <v-card color="primary" flat>
+                <v-card-text class="px-0 py-0">
+                    <h3 class="text-h4 text-center white--text">Cursuri</h3>
+                    <v-img src="<?= SITE_URL ?>/img/home/cursuri.jpg" width="100%" max-width="100vw" contain>
+                    </v-img>
+                </v-card-text>
+            </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+            <v-card color="primary" flat>
+                <v-card-text class="px-0 py-0">
+                    <h3 class="text-h4 text-center white--text">Lectori</h3>
+                    <v-img src="<?= SITE_URL ?>/img/home/lectori.png" width="100%" max-width="100vw" contain>
+                    </v-img>
+                </v-card-text>
+            </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+            <v-card color="primary" flat>
+                <v-card-text class="px-0 py-0">
+                    <h3 class="text-h4 text-center white--text">Despre noi</h3>
+                    <v-img src="<?= SITE_URL ?>/img/home/despre-noi.png" width="100%" max-width="100vw" contain>
+                    </v-img>
+                </v-card-text>
+            </v-card>
         </v-col>
     </v-row>
 </v-container>
